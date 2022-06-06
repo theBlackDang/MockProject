ed7558315747793e3d3ee92cbe328fd7411b4c39
theblackDang <nguyenhaidang.simple@gmail.com>
2022-06-06 19:30:00 +0700

    Spring initializing

---
+++ src/main/java/com/haidang/fpt/MockProjectApplication.java
@@ -0,0 +1,13 @@
+package com.haidang.fpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockProjectApplication.class, args);
+	}
+
+}
